db400e25d11e00f7801a94c3054682573947f24a
Zack Campbell <zajrik@users.noreply.github.com>
2025-01-22 22:08:01 -0600

    add remaining changes
    Stuff I forgot to commit that doesn't really matter at the moment

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -29,16 +29,15 @@ var player_direction := Vector2.ZERO
 var knockback_origin := Vector2.ZERO
 var is_knocked_back := false
 
+var facing: Enum.Facing = randi_range(0, Enum.Facing.size() - 1) as Enum.Facing
 
-
-
-var facing: Enum.Facing = randi_range(0, Enum.Facing.size() - 1)
 
 ## Emitted when the enemy is hit by an attack
 signal hit
 
 ## Emitted when the enemy is killed by an attack
 signal kill
+
 
 ## Draw debug information.
 func debug_draw() -> void:
@@ -47,8 +46,10 @@ func debug_draw() -> void:
         DebugDraw2D.set_text('enemy state', state_machine.state.name)
         DebugDraw2D.set_text('enemy attackable', state_machine.state_has_component(&'AttackableStateComponent'))
 
-func _process(delta: float) -> void:
+
+func _process(_delta: float) -> void:
     debug_draw()
+
 
 ## Set the direction the enemy is facing based on velocity angle.
 ## Facing will not be updated if velocity length is zero.
@@ -64,6 +65,7 @@ func face_velocity() -> void:
     else:
         facing = Enum.Facing.LEFT
 
+
 ## Handle movement of this enemy (it's own or knockback)
 func handle_movement() -> void:
     if is_knocked_back:
@@ -71,6 +73,7 @@ func handle_movement() -> void:
         set_velocity(knockback_direction.normalized() * speed * 4)
 
     move_and_slide()
+
 
 ## Attack this enemy
 func attack(damage: int, origin: Vector2) -> void:
@@ -88,6 +91,7 @@ func attack(damage: int, origin: Vector2) -> void:
 
     else:
         hit.emit()
+
 
 func _on_attack_area_mouse_entered() -> void:
     hovered = true
